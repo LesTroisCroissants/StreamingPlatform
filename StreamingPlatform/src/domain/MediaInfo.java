f9492934e514e6c38c8046a4536e47e1ce7bb859
Apple <oliha@itu.dk>
2022-12-01 15:18:27 +0100

    Update media registry for filter

--- StreamingPlatform/src/domain/MediaInfo.java
+++ StreamingPlatform/src/domain/MediaInfo.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public interface MediaInfo {
     List<Media> search(String input);
-    List<Media> filter(String category);
+    List<Media> filter(String category, int yearFrom, int yearTo, int rating);
     void initialize();
 }
--- StreamingPlatform/src/domain/MediaRegistry.java
+++ StreamingPlatform/src/domain/MediaRegistry.java
@@ -3,6 +3,8 @@ package domain;
 import java.util.List;
 import java.util.Set;
 
+import data.DataAccess;
+
 public class MediaRegistry implements MediaInfo {
     private List<Media> movies;
     private List<Media> series;
@@ -15,7 +17,7 @@ public class MediaRegistry implements MediaInfo {
     }
 
     @Override
-    public List<Media> filter(String category) {
+    public List<Media> filter(String category, int yearFrom, int yearTo, int rating) {
         // Implement filter function
         return null;
     }
@@ -23,7 +25,7 @@ public class MediaRegistry implements MediaInfo {
     @Override
     public void initialize() {
         // Get data from data-layer
-        
+        ;
     }
 
     public List<Media> getMovies() {
