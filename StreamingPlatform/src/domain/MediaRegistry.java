adebee0b07ebc0eeb5e993d94feff10ae20bb388
Apple <oliha@itu.dk>
2022-12-01 13:28:21 +0100

    Create data-registry

---
+++ StreamingPlatform/src/domain/MediaRegistry.java
@@ -0,0 +1,25 @@
+package domain;
+
+import java.util.List;
+
+public class MediaRegistry implements MediaInfo {
+
+    @Override
+    public List<Media> search(String input) {
+        // Implement search function
+        return null;
+    }
+
+    @Override
+    public List<Media> filter(String category) {
+        // Implement filter function
+        return null;
+    }
+
+    @Override
+    public void initialize() {
+        // Get data from data-layer
+        
+    }
+    
+}
