9634808c87cedf8df5453059ab858c0c3ce36e35
SnowyPeax <johbr@itu.dk>
2022-12-01 16:11:30 +0100

    Create classes in domain

---
+++ StreamingPlatform/src/domain/Season.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.util.List;
+
+/**
+ * Responsible for containing the Episode objects of a season.
+ */
+
+public class Season {
+
+    private final List<Episode> episodes;
+
+    public Season(List<Episode> episodes){
+        this.episodes = episodes;
+    }
+
+}
---
+++ StreamingPlatform/src/domain/Series.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * Responsible for containing the Season objects of a series
+ */
+
+public class Series extends Media{
+
+    private final List<Season> seasons;
+
+
+    public Series(String title, String year, Set<String> categories, double rating, Image coverImage, List<Season> seasons){
+        super(title, year, categories, rating, coverImage);
+
+        this.seasons = seasons;
+    }
+
+}
