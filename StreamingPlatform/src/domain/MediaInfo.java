df5876e2df84ec7df30d3adc68965dc44cccec4e
Apple <olivierb.hansen@gmail.com>
2022-12-06 17:37:18 +0100

    Create mediaRegistry and update MediaInfo also updated uml

--- StreamingPlatform/src/domain/MediaInfo.java
+++ StreamingPlatform/src/domain/MediaInfo.java
@@ -6,5 +6,4 @@ import java.util.List;
 public interface MediaInfo {
     List<Media> search(String input);
     List<Media> filter(String category, int yearFrom, int yearTo, int rating);
-    void initialize() throws FileNotFoundException;
 }
--- StreamingPlatform/src/domain/MediaRegistry.java
+++ StreamingPlatform/src/domain/MediaRegistry.java
@@ -1,16 +1,27 @@
 package domain;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import data.Data;
-import data.DataAccess;
+
+import javax.imageio.ImageIO;
 
 public class MediaRegistry implements MediaInfo {
     private List<Media> movies;
     private List<Media> series;
     private Set<String> categories;
+
+    public MediaRegistry() throws FileNotFoundException {
+        movies = new ArrayList<>();
+        series = new ArrayList<>();
+        initialize();
+    }
 
     @Override
     public List<Media> search(String input) {
@@ -24,18 +35,96 @@ public class MediaRegistry implements MediaInfo {
         return null;
     }
 
-    @Override
-    public void initialize() throws FileNotFoundException {
+    private void initialize() throws FileNotFoundException {
         Data data = new Data();
 
+        for (String movieInfo : data.getMovieInfo()) {
+            try {
+                movies.add(createMovie(movieInfo));
+            } catch (IOException e) {
+                System.out.println("Image path could not be resolved");
+            }
+        }
+
+        for (String seriesInfo : data.getSeriesInfo()) {
+            try {
+                series.add(createSeries(seriesInfo));
+            } catch (IOException e) {
+                System.out.println("Image path could not be resolved");
+            }
+        }
+
+        categories = data.getCategories();
     }
 
-    private void populateMovies(List<String> rawData) {
-        for (String movie : rawData) {
-            // Title, release year, category, rating, cover path
-            String[] properties = movie.split(";");
-            // Iterate movies
+    private Movie createMovie(String data) throws IOException {
+        // 0: Title, 1: release year, 2: category, 3: rating, 4: cover path
+        String[] properties = data.trim().split(";");
+
+        // Iterate movies
+        return new Movie(
+                properties[0],
+                properties[1],
+                parseCategories(properties[1]),
+                parseRating(properties[2]),
+                getCoverImage(properties[3]),
+                ""
+        );
+    }
+
+    private Series createSeries(String data) throws IOException {
+        // 0: Title, 1: release year, 2: category, 3: rating, 4: season-episode, 5: cover path
+        String[] properties = data.trim().split(";");
+
+        List<Season> seasons = new ArrayList<>();
+
+        String[] seasonsInfo = properties[4].trim().split(",");
+        for (String season : seasonsInfo) {
+            seasons.add(generateSeason(season));
         }
+
+        // Iterate movies
+        return new Series(
+                properties[0],
+                // TODO update to match Series should have from and to years
+                properties[1],
+                parseCategories(properties[1]),
+                parseRating(properties[2]),
+                getCoverImage(properties[3]),
+                seasons
+        );
+    }
+
+    private List<Episode> generateEpisodes(int amount) {
+        // TODO add episode numbers and description?
+        List<Episode> episodes = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            episodes.add(new Episode(""));
+        }
+
+        return episodes;
+    };
+
+    private Season generateSeason(String seasonInfo) {
+        int episodes = Integer.parseInt(seasonInfo.split("-")[1]);
+
+        return new Season(
+                generateEpisodes(episodes)
+        );
+    }
+
+    private Set<String> parseCategories(String property) {
+        return Set.of(property.trim().split(","));
+    }
+
+    private BufferedImage getCoverImage(String path) throws IOException {
+        File coverPath = new File(path);
+        return ImageIO.read(coverPath);
+    }
+
+    private Float parseRating(String property) {
+        return Float.parseFloat(property.replace(",","."));
     }
 
     public List<Media> getMovies() {
@@ -46,7 +135,7 @@ public class MediaRegistry implements MediaInfo {
         return series;
     }
 
-    public Set<String> getCategories() {
+    public Set<String> parseCategories() {
         return categories;
     }
     
